22814a97a425d8fa90f9b1ee2982f92f14c9aa21
wisdom0063 <kwartengwisdomug95@gmail.com>
2020-09-11 10:17:11 +0000

    allowing cors

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
 "use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const config = require("./config");
 const app = express();
 const controller = require("./controller");
 const { connectDb, logger } = require("./lib");
 const { sendError } = require("./middleware/error");
-const error = require("./middleware/error");
 app
   .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
+  .use(cors())
   .use(controller)
   .use((req, res) => res.status(404).send("Route not found"))
   .use(sendError);
-
 
 connectDb().then(() =>
   app.listen(config.get("PORT"), () => {
